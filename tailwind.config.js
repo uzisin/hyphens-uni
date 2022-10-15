c21b87abb88c17527a20c677ce94b240ca4c80ac
Khalid tajelsir <105322070+uzisin@users.noreply.github.com>
2022-10-15 06:08:01 -0700

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/index.html","./src/chat.html","./src/dashboard.html","./src/student-signin.html","./src/teacher-signin.html"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
